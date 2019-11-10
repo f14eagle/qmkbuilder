945c14107dd16cae79b14dfa017e56ca3e7b65be
BuildTools <unconfigured@null.spigotmc.org>
2019-11-10 12:50:15 +0800

    add qmk_firmware support

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const PORT = 5004;
 const TMP = '/tmp/qmk-';
+const QMK_PATH = '/msys64/home/f14eagle/gitrepo/qmk_firmware_f14'
 
 const Express = require('express');
 const BodyParser = require('body-parser');
@@ -35,8 +36,6 @@ app.post('/build', (req, res) => {
 	const key = Crypto.randomBytes(16).toString('hex');
 	console.log(`Tmp folder: ${ TMP + key }`)
 	const templateName = files.templateName ? files.templateName : 'default'
-	const templateDir = path.resolve(templateBasedir, templateName)
-
 	console.log(`Build firmware with templateName: ${ templateName }`)
 
 	// Setup helper functions.
@@ -52,63 +51,134 @@ app.post('/build', (req, res) => {
 
 	// Start.
 	co(function*() {
-		// Copy the base stencil.
-		yield new Promise((resolve, reject) => {
-			Exec('cp -rp qmk_firmware ' + TMP + key, (err, stdout, stderr) => {
-				if (err) return reject('Failed to initialize.');
-				resolve();
-			});
-		});
+		if(templateName == 'default'){
+			// If templateName is default, use original logic to generate firmware
 
-		// Copy all the files.
-		for (const file in files) {
+			// Copy the base stencil.
 			yield new Promise((resolve, reject) => {
-				const fileName = file.replace('qmk_firmware', TMP + key);
-				Fs.writeFile(fileName, files[file], err => {
+				Exec('cp -rp qmk_firmware ' + TMP + key, (err, stdout, stderr) => {
 					if (err) return reject('Failed to initialize.');
 					resolve();
 				});
 			});
-		}
 
-		// Copy template files if exists
-		yield new Promise((resolve, reject) => {
-			Fs.stat(templateDir, err => {
-				if(err){
-					console.log(`Template folder not exists: ${ templateDir }`)
-					resolve()
-				}else{
-					console.log(`Copy template files: ${ templateDir }`)
-					const targetDir = path.resolve(TMP + key, './keyboards/kb')
-					Exec('cp -rp ' + templateDir + '/* ' + targetDir + '/', (err, stdout, stderr) => {
-						if(err) return reject('Copy template files error')
+			// Copy all the files.
+			for (const file in files) {
+				yield new Promise((resolve, reject) => {
+					const fileName = file.replace('qmk_firmware', TMP + key);
+					Fs.writeFile(fileName, files[file], err => {
+						if (err) return reject('Failed to initialize.');
+						resolve();
+					});
+				});
+			}
+
+			// Copy template files if exists
+			yield new Promise((resolve, reject) => {
+				Fs.stat(templateDir, err => {
+					if(err){
+						console.log(`Template folder not exists: ${ templateDir }`)
 						resolve()
-					})
-				}
+					}else{
+						console.log(`Copy template files: ${ templateDir }`)
+						const targetDir = path.resolve(TMP + key, './keyboards/kb')
+						Exec('cp -rp ' + templateDir + '/* ' + targetDir + '/', (err, stdout, stderr) => {
+							if(err) return reject('Copy template files error')
+							resolve()
+						})
+					}
+				})
 			})
-		})
 
-		// Make.
-		yield new Promise((resolve, reject) => {
-			Exec('cd ' + TMP + key + '/keyboards/kb && make', (err, stdout, stderr) => {
-				if (err) return reject(stderr);
-				resolve();
+			// Make.
+			yield new Promise((resolve, reject) => {
+				Exec('cd ' + TMP + key + '/keyboards/kb && make', (err, stdout, stderr) => {
+					if (err) return reject(stderr);
+					resolve();
+				});
 			});
-		});
 
-		// Read the hex file.
-		const hex = yield new Promise((resolve, reject) => {
-			Fs.readFile(TMP + key + '/kb_default.hex', 'utf8', (err, data) => {
-				if (err) return reject('Failed to read hex file.');
-				resolve(data);
+			// Read the hex file.
+			const hex = yield new Promise((resolve, reject) => {
+				Fs.readFile(TMP + key + '/kb_default.hex', 'utf8', (err, data) => {
+					if (err) return reject('Failed to read hex file.');
+					resolve(data);
+				});
 			});
-		});
 
-		// Send the hex file.
-		res.json({ hex: hex });
+			// Send the hex file.
+			res.json({ hex: hex });
 
-		// Clean up.
-		// clean();
+			// Clean up.
+			// clean();
+		}else{
+			// If user specified templateName, use qmk_firmware env to build
+			const firmwareName = 'redox_f14'
+			const keymapFolder = 'bft'
+			const targetKeymap = 'kb-' + key
+			const firmwareBasePath = path.resolve(QMK_PATH, './keyboards', firmwareName)
+			const keymapBasePath = path.resolve(firmwareBasePath, './keymaps')
+			const keymapPath = path.resolve(keymapBasePath, keymapFolder)
+			const targetPath = path.resolve(keymapBasePath, targetKeymap)
+
+			// Copy the base stencil.
+			yield new Promise((resolve, reject) => {
+				Exec(`cp -rp ${ keymapPath } ${ targetPath }`, (err, stdout, stderr) => {
+					if (err){
+						console.log(err)
+						return reject('Failed to create target keymap directory')
+					} 
+					resolve();
+				});
+			});
+
+			// Copy keymap file onlye
+			for (const file in files) {
+				yield new Promise((resolve, reject) => {
+					// Only process keymap file
+					if(!file.includes('keymap')){
+						return resolve()
+					}
+					const filename = targetPath + '/keymap.c'
+					const filecontent = files[file].replace('kb.h', `${ firmwareName }.h`)
+					Fs.writeFile(filename, filecontent, err => {
+						if (err){
+							console.log(err)
+							return reject('Failed to write file from request')
+						} 
+						resolve();
+					});
+				});
+			}
+
+			// Make.
+			yield new Promise((resolve, reject) => {
+				Exec(`cd ${ QMK_PATH } && make ${ firmwareName }:${ targetKeymap }`, (err, stdout, stderr) => {
+					if (err){
+						console.log(err)
+						return reject(stderr);
+					}
+					resolve();
+				});
+			});
+
+			// Read the hex file.
+			const hex = yield new Promise((resolve, reject) => {
+				const hexfile = `${ QMK_PATH }/${ firmwareName }_${ targetKeymap }.hex`
+				console.log(`Read hex file: ${ hexfile }`)
+				Fs.readFile(hexfile, 'utf8', (err, data) => {
+					if (err){
+						console.log(err)
+						return reject('Failed to read hex file.');
+					}
+					resolve(data);
+				});
+			});
+
+			// Send the hex file.
+			res.json({ hex: hex });
+		}
+		
 	}).catch(e => sendError(e));
 });
 
