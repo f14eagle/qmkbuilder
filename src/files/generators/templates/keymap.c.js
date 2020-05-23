b7621d43da982dfdc5cd1b7f88ef4525f93c79bf
BuildTools <unconfigured@null.spigotmc.org>
2020-05-23 22:43:54 +0800

    remove event variable from macro

--- src/files/generators/templates/keymap.c.js
+++ src/files/generators/templates/keymap.c.js
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //--macro-start
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-	keyevent_t event = record->event;
+	//keyevent_t event = record->event;
 
 	switch (id) {
 %macros%
